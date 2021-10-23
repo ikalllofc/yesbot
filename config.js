8a18d4dbc2338b8b9601f6f61ccf5c857a177986
ikalllofc <92963826+ikalllofc@users.noreply.github.com>
2021-10-23 21:50:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 userbot = {
  owner: [
-  "62822980698996", 
-  "6282328303332",
-  "62823283033323"
+  "6289696454872", 
+  "6289696454872",
+  "6289696454872"
   ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
    error: "errorr!!"
    },
-   prefix: ["#"],
+   prefix: ["."],
     setting: {
     admin: "only admin",
     group: "only group",
